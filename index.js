22a0fe755969f05118fe9bf040c9612b5dfaef09
Stalin Bonilla <sbonilla@dominet.net>
2014-07-16 13:41:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var btSerial = new(require('bluetooth-serial-port')).BluetoothSerialPort();
+var exec = require('child_process').exec;
+var isFound = false;
+var isLock = false;
+var device = 'E8:99:C4:58:CE:D7';
+btSerial.on('found', function (address, name) {
+    if (address === device) {
+        isFound = true;
+        console.log('device ' + name + ' found');
+    }
+});
+btSerial.on('finished', function () {
+    if (isFound && isLock) {
+        exec('gnome-screensaver-command -d', function (error, stdout, stderr) {
+            console.log('unlocking...');
+            isLock = false;
+        });
+    } else {
+        exec('gnome-screensaver-command -l', function (error, stdout, stderr) {
+            console.log('blocking...');
+            isLock = true;
+        });
+
+    }
+    setTimeout(function () {
+        isFound = false;
+        btSerial.inquire();
+    }, 2000);
+});
+console.log('searching for devices');
+btSerial.inquire();
---
+++ readme.md
@@ -0,0 +1,10 @@
+#Blocktooth
+
+
+Esta aplicacion desbloquea y bloquea un equipo basado en la cercania de otro disposivo, lo hace a travez de bluetooth
+
+####Uso
+
+Solo cambia la variable `device` con la mac address del dispositivo que quieras
+
+> nota: solo ha sido probado en ubuntu 12.04 ademas es necesario instalar en ubuntu libbluetooth-dev `sudo apt-get install libbluetooth-dev`
